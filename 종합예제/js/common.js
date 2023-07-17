16196c472f2a0d4cbaea07fcead7324cfe54bffc
UICHEOL_HAWNG <icuchoel@gmail.com>
2023-07-17 18:16:17 +0900

    5차

---
+++ 종합예제/js/common.js
@@ -0,0 +1,22 @@
+// 조작할 대상을 변수에 담기
+const btnTop =
+  document.querySelector(".btn-top"); /* btn-top이라는 태그를 선택해라 */
+const html = document.documentElement; /* 웹문서의 요소를 골라라*/
+const htmlPos = html.scrollHeight / 2;
+
+console.log(htmlPos);
+// 조작할 상황을 생각해보기
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY;
+  console.log(scrollTop); /* 스크롤 횟수 측정 */
+
+  if (scrollTop >= htmlPos) {
+    /* 스크롤 값이 문서 세로크기의 절반 값을 넘어가면  */
+    btnTop.classList.add("active"); /* active클래스 부여 */
+  } else {
+    btnTop.classList.remove("active"); /* active 클래스 제거 */
+  }
+  // btnTop에 active라는 클래스를 부여시킨다.
+}); /* 창에 스크롤이 생겼을 때  */
+
+// 동작을 생각해보기
